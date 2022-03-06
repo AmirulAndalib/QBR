b61288e965d763b4ff85bdc8c3ee6f4269153d03
AmirulAndalib <amirulandalib9@gmail.com>
2022-03-06 20:39:14 +0600

    liek liek

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
-echo "Scripted By DevAnaZ & Andalib"
+
+git clone https://github.com/AmirulAndalib/QBR
+
+cd QBR
+
+git clone --depth 1 --single-branch --branch latest-release https://github.com/WDaan/VueTorrent.git QBR/.VueTorrent
+
+ls
+
 mkdir .config
 mkdir .config/rclone
+
+chmod +x .config
+chmod+x qBittorrent
+chmod+x qBittorrent/qBittorrent.conf
+chmod +x  qBittorrent/config/qBittorrent.conf
+chmod +x qBittorrent/WEBHASHPASSWORD
+chmod 777 qBittorrent/WEBHASHPASSWORD
+
 curl -L $RCLONE_CONFIG_IN_RAW_URL >.config/rclone/rclone.conf
 echo $PORT >/PORT
-#curl -L $QBITTORRENT_CONFIG_IN_RAW_URL
-git clone --depth 1 --single-branch --branch latest-release https://github.com/WDaan/VueTorrent.git /content/.VueTorrent
 
 cat qBittorrent/config/qBittorrent.conf >qBittorrent/qBittorrent.conf
-echo -n "$WEBPASSWORD" |md5sum |sed 's|  -||g' >qBittorrent/WEBHASHPASSWORD
-cat qBittorrent/qBittorrent.conf |sed "s|devanaz:|$(rclone listremotes |head -1)|g" |sed "s|developeranaz|$WEBUSER|g" |sed "s|bc89e64478e74b30bea280349e1220ce|$(cat /qBittorrent/WEBHASHPASSWORD)|g" >/qBittorrent/config/qBittorrent.conf
+
+echo -n "$WEBPASSWORD" |md5sum |sed 's|  -||g' >/qBittorrent/WEBHASHPASSWORD
+
+cat qBittorrent/qBittorrent.conf |sed "s|devanaz:|$(rclone listremotes |head -1)|g" |sed "s|developeranaz|$WEBUSER|g" |sed "s|bc89e64478e74b30bea280349e1220ce|$(cat /qBittorrent/WEBHASHPASSWORD)|g" >qBittorrent/config/qBittorrent.conf
+
 
 while :
 do
-qbittorrent-nox --profile=/ --webui-port="$(cat /PORT)"
+qbittorrent-nox --profile= .--webui-port="$(cat /PORT)"
 done
