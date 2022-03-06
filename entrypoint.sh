a3eeb8300e533244db41d74f3903ad44c895c1b9
AmirulAndalib <amirulandalib9@gmail.com>
2022-03-06 20:54:20 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ git clone https://github.com/AmirulAndalib/QBR
 
 cd QBR
 
-git clone --depth 1 --single-branch --branch latest-release https://github.com/WDaan/VueTorrent.git QBR/.VueTorrent
+git clone --depth 1 --single-branch --branch latest-release https://github.com/WDaan/VueTorrent.git .VueTorrent
 
 ls
 
@@ -15,17 +15,17 @@ chmod +x .config
 chmod+x qBittorrent
 chmod+x qBittorrent/qBittorrent.conf
 chmod +x  qBittorrent/config/qBittorrent.conf
-chmod +x qBittorrent/WEBHASHPASSWORD
-chmod 777 qBittorrent/WEBHASHPASSWORD
 
 curl -L $RCLONE_CONFIG_IN_RAW_URL >.config/rclone/rclone.conf
 echo $PORT >/PORT
 
 cat qBittorrent/config/qBittorrent.conf >qBittorrent/qBittorrent.conf
 
-echo -n "$WEBPASSWORD" |md5sum |sed 's|  -||g' >/qBittorrent/WEBHASHPASSWORD
+echo -n "$WEBPASSWORD" |md5sum |sed 's|  -||g' >WEBHASHPASSWORD && mv WEBHASHPASSWORD /qBittorrent
 
-cat qBittorrent/qBittorrent.conf |sed "s|devanaz:|$(rclone listremotes |head -1)|g" |sed "s|developeranaz|$WEBUSER|g" |sed "s|bc89e64478e74b30bea280349e1220ce|$(cat /qBittorrent/WEBHASHPASSWORD)|g" >qBittorrent/config/qBittorrent.conf
+cat qBittorrent/qBittorrent.conf |sed "s|devanaz:|$(rclone listremotes |head -1)|g" |
+
+sed "s|developeranaz|$WEBUSER|g" |sed "s|bc89e64478e74b30bea280349e1220ce|$(cat qBittorrent/WEBHASHPASSWORD)|g" >qBittorrent/config/qBittorrent.conf
 
 
 while :
